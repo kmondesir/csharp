e73159ca3637c1b7f6f09e29462fb9f004dd7c59
Kino Mondesir <kino.mondesir@gmail.com>
2020-06-13 06:56:51 -0400

    update Program

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,7 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello King!");
+            Console.WriteLine($"Hello {args[0]}");
         }
     }
 }
