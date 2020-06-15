30e7cb9c356a1235773e25ee820e8a6788c48715
Kino Mondesir <kino.mondesir@gmail.com>
2020-06-14 20:34:33 -0400

    Added grades field

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -1,10 +1,12 @@
+using System.Collections.Generic;
 namespace GradeBook
 {
   class Book 
   {
+    List<double> grades;
     public void Add(double grade)
     {
-      
+      grades.Add(grade);
     }
   }
 }
